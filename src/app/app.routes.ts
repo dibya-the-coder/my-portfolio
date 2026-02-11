f3ef2f9eee667eb37ad0ce6bf9c2f65482403627
DIBYA RANJAN ROUT <dibyaranjan.developer@gmail.com>
2026-02-11 21:24:41 +0530

    added for -deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,7 +32,6 @@ export const routes: Routes = [
     //     path: 'achievements',
     //     component: AchievementsComponent
     // },
-
     {
         path: '**',
         component: NotFoundComponent
